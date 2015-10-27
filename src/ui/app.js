e11b700725e589a7e709f22c73f6bee399fe7542
Tim Officer <timothyofficer@gmail.com>
2015-10-27 14:39:21 -0400

    initialize keypress events in a keypress run block instead of main run block

--- src/ui/app.js
+++ src/ui/app.js
@@ -12,22 +12,17 @@ angular.module('app', [
 angular.module('app').run([
   '$rootScope',
   '$log',
-  '$timeout',
-  'alertService',
-  'keypressService',
   'modalService',
   'tabCache',
-  function AppCtrl($rootScope, $log, $timeout, alertService, keypressService, modalService, tabCache) {
+  function ($rootScope, $log, modalService, tabCache) {
     const ipc = require('ipc');
-    const urlHelpers = require('lib/helpers/urlHelpers');
-
-    registerKeypressEvents();
+    // const urlHelpers = require('lib/helpers/urlHelpers');
 
     $rootScope.themes = initThemes();
 
-    $rootScope.getBrowserUrl = function(url) {
-      return urlHelpers.getBrowserUrl(url);
-    };
+    // $rootScope.getBrowserUrl = function(url) {
+    //   return urlHelpers.getBrowserUrl(url);
+    // };
 
     $rootScope.setTitle = function(title) {
       ipc.send('set-title', title);
@@ -60,36 +55,6 @@ angular.module('app').run([
         tabCache.activateByName(settingsTabName);
       }
     };
-
-    function registerKeypressEvents() {
-      // unregister keypress events on scope destroy
-      $rootScope.$on('$destroy', function() {
-        keypressService.unregisterCombo(keypressService.EVENTS.CLOSE_WINDOW);
-        keypressService.unregisterCombo(keypressService.EVENTS.MOVE_LEFT);
-        keypressService.unregisterCombo(keypressService.EVENTS.MOVE_RIGHT);
-        keypressService.unregisterCombo(keypressService.EVENTS.OPEN_CONNECTION_MANAGER);
-      });
-
-      keypressService.registerCombo(keypressService.EVENTS.CLOSE_WINDOW, function() {
-        console.log(keypressService.EVENTS.CLOSE_WINDOW);
-        tabCache.removeActive();
-      });
-
-      keypressService.registerCombo(keypressService.EVENTS.MOVE_LEFT, function() {
-        console.log(keypressService.EVENTS.MOVE_LEFT);
-        tabCache.activatePrevious();
-      });
-
-      keypressService.registerCombo(keypressService.EVENTS.MOVE_RIGHT, function() {
-        console.log(keypressService.EVENTS.MOVE_RIGHT);
-        tabCache.activateNext();
-      });
-
-      keypressService.registerCombo(keypressService.EVENTS.OPEN_CONNECTION_MANAGER, function() {
-        console.log(keypressService.EVENTS.OPEN_CONNECTION_MANAGER);
-        $rootScope.showConnections();
-      });
-    }
 
     function initThemes() {
       return {
--- src/ui/services/keypressService.js
+++ src/ui/services/keypressService.js
@@ -1,45 +1,78 @@
 angular.module('app').factory('keypressService', [
-  '$window',
-  '$rootScope',
-  '$log',
-  function($window, $rootScope, $log) {
+    '$window',
+    '$rootScope',
+    '$log',
+    function($window, $rootScope, $log) {
 
-    function KeypressService() {
-      this.registeredCombos = [];
-      this.listener = new $window.keypress.Listener();
-    }
-
-    KeypressService.prototype.isRegistered = function isRegistered(combo) {
-      var registration = this.registeredCombos[combo];
-      return registration ? true : false;
-    };
-
-    KeypressService.prototype.registerCombo = function registerCombo(combo, callback) {
-      if (this.isRegistered(combo)) {
-        $log.error(combo + ' is already registered, unregistering previous combo');
-        this.unregisterCombo(combo);
+      function KeypressService() {
+        this.registeredCombos = [];
+        this.listener = new $window.keypress.Listener();
       }
 
-      $log.debug('Registering keypress combo : ' + combo);
+      KeypressService.prototype.isRegistered = function isRegistered(combo) {
+        var registration = this.registeredCombos[combo];
+        return registration ? true : false;
+      };
 
-      this.listener.counting_combo(combo, function() { // jshint ignore:line
-        callback.call(arguments);
-        $rootScope.$apply();
-      }, true);
-    };
+      KeypressService.prototype.registerCombo = function registerCombo(combo, callback) {
+        if (this.isRegistered(combo)) {
+          $log.error(combo + ' is already registered, unregistering previous combo');
+          this.unregisterCombo(combo);
+        }
 
-    KeypressService.prototype.unregisterCombo = function unregisterCombo(combo) {
-      this.listener.unregister_combo(combo); // jshint ignore:line
-    };
+        $log.debug('Registering keypress combo : ' + combo);
 
-    KeypressService.prototype.EVENTS = {
-      CLOSE_WINDOW: 'meta w',
-      NEW_TAB: 'meta t',
-      MOVE_LEFT: 'meta shift {',
-      MOVE_RIGHT: 'meta shift }',
-      OPEN_CONNECTION_MANAGER: 'meta shift n'
-    };
+        this.listener.counting_combo(combo, function() { // jshint ignore:line
+          callback.call(arguments);
+          $rootScope.$apply();
+        }, true);
+      };
 
-    return new KeypressService();
-  }
-]);
+      KeypressService.prototype.unregisterCombo = function unregisterCombo(combo) {
+        this.listener.unregister_combo(combo); // jshint ignore:line
+      };
+
+      KeypressService.prototype.EVENTS = {
+        CLOSE_WINDOW: 'meta w',
+        NEW_TAB: 'meta t',
+        MOVE_LEFT: 'meta shift {',
+        MOVE_RIGHT: 'meta shift }',
+        OPEN_CONNECTION_MANAGER: 'meta shift n'
+      };
+
+      return new KeypressService();
+    }
+  ])
+  .run([
+    '$rootScope',
+    'keypressService',
+    'tabCache',
+    function($rootScope, keypressService, tabCache) {
+      $rootScope.$on('$destroy', function() {
+        keypressService.unregisterCombo(keypressService.EVENTS.CLOSE_WINDOW);
+        keypressService.unregisterCombo(keypressService.EVENTS.MOVE_LEFT);
+        keypressService.unregisterCombo(keypressService.EVENTS.MOVE_RIGHT);
+        keypressService.unregisterCombo(keypressService.EVENTS.OPEN_CONNECTION_MANAGER);
+      });
+
+      keypressService.registerCombo(keypressService.EVENTS.CLOSE_WINDOW, function() {
+        console.log(keypressService.EVENTS.CLOSE_WINDOW);
+        tabCache.removeActive();
+      });
+
+      keypressService.registerCombo(keypressService.EVENTS.MOVE_LEFT, function() {
+        console.log(keypressService.EVENTS.MOVE_LEFT);
+        tabCache.activatePrevious();
+      });
+
+      keypressService.registerCombo(keypressService.EVENTS.MOVE_RIGHT, function() {
+        console.log(keypressService.EVENTS.MOVE_RIGHT);
+        tabCache.activateNext();
+      });
+
+      keypressService.registerCombo(keypressService.EVENTS.OPEN_CONNECTION_MANAGER, function() {
+        console.log(keypressService.EVENTS.OPEN_CONNECTION_MANAGER);
+        $rootScope.showConnections();
+      });
+    }
+  ]);
