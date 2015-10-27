43e0c4df2928455f10cf96eece465ac6e218282a
Tim Officer <timothyofficer@gmail.com>
2015-10-27 17:22:04 -0400

    add a custom menu

--- src/ui/services/menuService.js
+++ src/ui/services/menuService.js
@@ -1,12 +1,14 @@
 'use strict';
+var ipc = require('ipc');
+var shell = require('shell');
+var remote = require('remote');
+var Menu = remote.require('menu');
+var MenuItem = remote.require('menu-item');
+var appConfig = require('src/config/appConfig');
 
 angular.module('app').factory('menuService', [
   '$window',
   function($window) {
-    const remote = require('remote');
-    const Menu = remote.require('menu');
-    const MenuItem = remote.require('menu-item');
-
     let MENU_CACHE = {};
 
     function MenuService() {
@@ -57,5 +59,157 @@ angular.module('app').factory('menuService', [
     };
 
     return new MenuService();
+  }
+]);
+
+angular.module('app').run([
+  '$rootScope',
+  '$timeout',
+  function($rootScope, $timeout) {
+    var template = [{
+      label: 'Edit',
+      submenu: [{
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      }, {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      }, {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      }, {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      }, {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      }, ]
+    }, {
+      label: 'View',
+      submenu: [{
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.reload();
+        }
+      }, {
+        label: 'Toggle Full Screen',
+        accelerator: (function() {
+          if (process.platform === 'darwin')
+            return 'Ctrl+Command+F';
+          else
+            return 'F11';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      }, {
+        label: 'Toggle Developer Tools',
+        accelerator: (function() {
+          if (process.platform === 'darwin')
+            return 'Alt+Command+I';
+          else
+            return 'Ctrl+Shift+I';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.toggleDevTools();
+        }
+      }, ]
+    }, {
+      label: 'Window',
+      role: 'window',
+      submenu: [{
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      }, {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      }, ]
+    }, {
+      label: 'Help',
+      role: 'help',
+      submenu: [{
+        label: 'Learn More',
+        click: function() {
+          shell.openExternal(appConfig.repository);
+        }
+      }, ]
+    }, ];
+
+    if (process.platform === 'darwin') {
+      var name = appConfig.name;
+
+      template.unshift({
+        label: name,
+        submenu: [{
+          label: 'About ' + name,
+          role: 'about'
+        }, {
+          type: 'separator'
+        }, {
+          label: 'Preferences...',
+          click: function() {
+            $timeout(function() {
+              $rootScope.showSettings();
+            });
+          }
+        }, {
+          type: 'separator'
+        }, {
+          label: 'Services',
+          role: 'services',
+          submenu: []
+        }, {
+          type: 'separator'
+        }, {
+          label: 'Hide ' + name,
+          accelerator: 'Command+H',
+          role: 'hide'
+        }, {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          role: 'hideothers'
+        }, {
+          label: 'Show All',
+          role: 'unhide'
+        }, {
+          type: 'separator'
+        }, {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function() {
+            ipc.send('quit');
+          }
+        }, ]
+      });
+
+      // Window menu.
+      template[3].submenu.push({
+        type: 'separator'
+      }, {
+        label: 'Bring All to Front',
+        role: 'front'
+      });
+    }
+
+    var menu = Menu.buildFromTemplate(template);
+
+    Menu.setApplicationMenu(menu);
   }
 ]);
