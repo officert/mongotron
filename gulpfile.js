85948400f238ba177101c44d75daf97a2b459492
Tim Officer <timothyofficer@gmail.com>
2015-11-17 10:28:24 -0500

    re-work gulp-release task to switch symlinks to node_modules directory - this is needed because when we compile ES6 => ES5 we need to change which directory our symlinks points to (they should point to the compiled build dir)

--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,13 @@ const _ = require('underscore');
 const childProcess = require('child_process');
 const babel = require('gulp-babel');
 
-const appConfig = require('src/config/appConfig');
+const appConfig = require('./src/config/appConfig');
 
 require('gulp-task-list')(gulp);
 
 /* =========================================================================
  * Constants
  * ========================================================================= */
-const APP_NAME = packageJson.name;
-
 const SRC_DIR = 'src';
 const DOCS_DIR = 'docs';
 const RELEASE_IGNORE_PKGS = [ //any npm packages that should not be included in the release
@@ -89,47 +87,52 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('release', function() {
-  runSequence('build', 'pre-release', function() {
-    var env = _.extend({}, process.env);
-    env.NODE_ENV = 'production';
+gulp.task('release', ['pre-release'], function() {
+  // Build Steps:
+  //-------------------------------------
+  // run build to cleanup dirs and compile less
+  // run babel to compile ES6 => ES5
+  // run prod-sym-links to change symlinks in node_modules that point to src dir to the build dir (which will contain the compiled ES5 code)
+  //-------------------------------------
+  var env = _.extend({}, process.env);
+  env.NODE_ENV = 'production';
 
-    var child = childProcess.spawn('./node_modules/.bin/electron-packager', [
-      '.',
-      APP_NAME,
-      '--out',
-      appConfig.releasePath,
-      '--platform',
-      'darwin',
-      '--arch',
-      'x64',
-      '--version',
-      '0.30.2',
-      '--ignore', ('node_modules/(' + RELEASE_IGNORE_PKGS + ')'),
-      '--icon',
-      RELEASE_IMAGE_ICON,
-      '--appPath',
-      'build/browser/main.js'
-    ], {
-      env: env
-    });
+  var child = childProcess.spawn('./node_modules/.bin/electron-packager', [
+    '.',
+    appConfig.name,
+    '--out',
+    appConfig.releasePath,
+    '--platform',
+    'darwin',
+    '--arch',
+    'x64',
+    '--version',
+    '0.30.2',
+    '--ignore', ('node_modules/(' + RELEASE_IGNORE_PKGS + ')'),
+    '--icon',
+    RELEASE_IMAGE_ICON,
+    '--appPath',
+    'build/browser/main.js'
+  ], {
+    env: env
+  });
 
-    child.stdout.on('data',
-      function(data) {
-        console.log('tail output: ' + data);
-      }
-    );
+  child.stdout.on('data',
+    function(data) {
+      console.log('tail output: ' + data);
+    }
+  );
 
-    child.on('exit', function(exitCode) {
-      console.log('Child exited with code: ' + exitCode);
-    });
+  child.on('exit', function(exitCode) {
+    console.log('Child exited with code: ' + exitCode);
   });
 });
 
-/*
- * @task pre-release - compile ES6 to ES5 using babel
- */
-gulp.task('pre-release', function() {
+gulp.task('pre-release', function(next) {
+  runSequence('build', 'babel', next);
+});
+
+gulp.task('babel', function() {
 
   _init(gulp.src(['src/**/*.js', '!src/ui/vendor/**/*.*']))
     .pipe(babel({
@@ -138,7 +141,31 @@ gulp.task('pre-release', function() {
     .pipe(gulp.dest(appConfig.buildPath));
 });
 
-gulp.task('build', ['clean', 'css']);
+gulp.task('prod-sym-links', function(next) {
+
+  childProcess.exec('make createProductionSymLinks', function(err, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (err !== null) {
+      console.log('exec error: ' + err);
+    }
+
+    return next(err);
+  });
+});
+
+gulp.task('dev-sym-links', function() {
+
+  childProcess.exec('make createDevelopmentSymLinks', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+});
+
+gulp.task('build', ['clean', 'css', 'dev-sym-links']);
 
 gulp.task('serve', ['build'], function() {
 
