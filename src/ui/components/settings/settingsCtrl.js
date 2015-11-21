8a85710e20dfb24d12c45e068c2425cb07bb01c1
Tim Officer <timothyofficer@gmail.com>
2015-11-21 09:28:51 -0500

    settings tab styles

--- src/ui/components/settings/settingsCtrl.js
+++ src/ui/components/settings/settingsCtrl.js
@@ -5,7 +5,7 @@ angular.module('app').controller('settingsCtrl', [
 
     $scope.preferencesSrc = __dirname + '/components/settings/preferences/preferences.html';
     $scope.keybindingsSrc = __dirname + '/components/settings/keybindings/keybindings.html';
-    $scope.themesSrc = __dirname + '/components/settings/keybindings/themes.html';
+    $scope.themesSrc = __dirname + '/components/settings/themes/themes.html';
 
     $scope.currentPage = 'preferences';
   }
