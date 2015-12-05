1ffe0e39c52eee0f39cd94d30fd283e947d486d7
Tim Officer <timothyofficer@gmail.com>
2015-12-04 23:34:43 -0500

    on export button click, run find query as stream and write it to a CSV file

--- src/ui/directives/queryResultsExport/queryResultsExportCtrl.js
+++ src/ui/directives/queryResultsExport/queryResultsExportCtrl.js
@@ -1,8 +1,19 @@
+'use strict';
+
 angular.module('app').controller('queryResultsExportCtrl', [
   '$scope',
   'dialogService',
   '$log',
-  function($scope, dialogService, $log) {
+  '$timeout',
+  'alertService',
+  function($scope, dialogService, $log, $timeout, alertService) {
+    if (!$scope.collection) throw new Error('queryResultsExportCtrl - collection is required on scope');
+    if (!$scope.results) throw new Error('queryResultsExportCtrl - results is required on scope');
+    if (!$scope.query) throw new Error('queryResultsExportCtrl - query is required on scope');
+
+    const fs = require('fs');
+    const CsvStream = require('lib/utils/csvStream');
+
     $scope.keyValuePairs = [];
 
     $scope.handle = $scope.handle || {};
@@ -27,13 +38,53 @@ angular.module('app').controller('queryResultsExportCtrl', [
     };
 
     function _export() {
+      let nameProps = $scope.keyValuePairs.map((kvp) => {
+        return {
+          name: kvp.key,
+          property: kvp.value
+        };
+      });
+
       dialogService.showSaveDialog()
         .then((path) => {
-          alert(path);
+          path = _fixExportCsvPath(path);
+
+          $timeout(() => {
+            $scope.exporting = true;
+
+            $scope.collection.find($scope.query, {
+                stream: true
+              })
+              .then((stream) => {
+                stream
+                  .pipe(new CsvStream(nameProps))
+                  .on('error', (error) => {
+                    $log.error(error);
+                  })
+                  .pipe(fs.createWriteStream(path))
+                  .on('error', (error) => {
+                    $log.error(error);
+                  })
+                  .on('finish', () => {
+                    $timeout(() => {
+                      $scope.exporting = false;
+                      alertService.success('Finished exporting');
+                    });
+                  });
+              })
+              .catch((error) => {
+                $log.error(error);
+              });
+          });
         })
         .catch((err) => {
           $log.error(err);
         });
+    }
+
+    function _fixExportCsvPath(path) {
+      if (path.indexOf('.csv') < 0) path += '.csv';
+      return path;
     }
 
     function _saveExportConfig() {
