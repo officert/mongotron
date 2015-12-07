a126d508643c3c6532706a25994ed81244f394b9
Tim Officer <timothyofficer@gmail.com>
2015-12-07 15:02:45 -0500

    bunch of tests for Evaluator.js

--- tests/unit/modules/query/evaluator/eval-test.js
+++ tests/unit/modules/query/evaluator/eval-test.js
@@ -54,7 +54,7 @@ describe('modules', function() {
           });
         });
 
-        describe.only('when expression passed is a string', function() {
+        describe('when expression passed is a string', function() {
           describe('simple object expression', function() {
             it('should evaluate the expression and return the result', function(next) {
               var expression = '{ foobar : 1 }';
@@ -63,6 +63,63 @@ describe('modules', function() {
               };
 
               var result = evaluator.eval(expression);
+
+              _.isError(result).should.equal(false);
+
+              testUtils.compareObjects(result, expectedResult).should.equal(true);
+
+              return next(null);
+            });
+          });
+
+          describe('simple object expression with date', function() {
+            it('should evaluate the expression and return the result', function(next) {
+              var expression = '{ foobar1 : 1, foobar2: 2, foobar3: 3, date : new Date(2015, 1) }';
+              var expectedResult = {
+                foobar1: 1,
+                foobar2: 2,
+                foobar3: 3,
+                date: new Date(2015, 1)
+              };
+
+              var result = evaluator.eval(expression);
+
+              _.isError(result).should.equal(false);
+
+              testUtils.compareObjects(result, expectedResult).should.equal(true);
+
+              return next(null);
+            });
+          });
+
+          describe('expression that attempts to use a function that is not in scope', function() {
+            it('should return an error', function(next) {
+              var expression = '{ foobar : someFunction() }';
+              var scope = {};
+
+              var result = evaluator.eval(expression, scope);
+
+              _.isError(result).should.equal(true);
+
+              result.message.should.equal('someFunction is not defined');
+
+              return next(null);
+            });
+          });
+
+          describe('expression that attempts to use a function that is in scope', function() {
+            it('should evaluate the expression and return the result', function(next) {
+              var expression = '{ foobar : someFunction() }';
+              var scope = {
+                someFunction: function() {
+                  return 'Hello';
+                }
+              };
+              var expectedResult = {
+                foobar: scope.someFunction()
+              };
+
+              var result = evaluator.eval(expression, scope);
 
               _.isError(result).should.equal(false);
 
