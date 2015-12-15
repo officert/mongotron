bb9bacb1a8728f0744831133a20415c9a31d82a8
Tim Officer <timothyofficer@gmail.com>
2015-12-15 07:05:11 -0500

    add flex box layout to the query form and result so that the editor can scroll after 220px, and the result always take up 100% of the space below

--- src/ui/directives/loading.js
+++ src/ui/directives/loading.js
@@ -3,7 +3,10 @@ angular.module('app').directive('loading', [
     return {
       restrict: 'E',
       replace: true,
-      template: '<p class="loading center" ng-show="loading"><span><i class="fa fa-cog fa-spin fa-2x"></i></span></p>'
+      template: '<p class="loading center" ng-show="loading"><span><i class="fa fa-cog fa-spin fa-2x"></i></span></p>',
+      scope: {
+        loading: '='
+      }
     };
   }
 ]);
