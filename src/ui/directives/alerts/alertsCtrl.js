d788725d3f6042e54c6bf07a63fc93de1a24bd42
Tim Officer <timothyofficer@gmail.com>
2015-12-19 09:36:43 -0500

    add a timeout and remove alerts

--- src/ui/directives/alerts/alertsCtrl.js
+++ src/ui/directives/alerts/alertsCtrl.js
@@ -5,11 +5,17 @@ angular.module('app').controller('alertsCtrl', [
   'alertService',
   '$timeout',
   function($scope, alertService, $timeout) {
+    const ALERT_TIMEOUT = 4000;
+
     $scope.alerts = [];
 
     alertService.on(alertService.EVENTS.NEW_ALERT, (alert) => {
       $timeout(() => {
         $scope.alerts.push(alert);
+        $timeout(() => {
+          var index = $scope.alerts.indexOf(alert);
+          $scope.alerts.splice(index, 1);
+        }, ALERT_TIMEOUT);
       });
     });
 
