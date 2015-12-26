08ecc4deaf570698d2a2c2be906d133c8879efe9
Tim Officer <timothyofficer@gmail.com>
2015-12-26 15:36:20 -0500

    don’t use RegEx to extract objects from query string, instead use the new bracket matcher class

--- src/lib/modules/query/parser.js
+++ src/lib/modules/query/parser.js
@@ -1,11 +1,10 @@
 'use strict';
 
+const bracketMatcher = require('./bracketMatcher');
+
 const VALID_QUERY_REGEX = new RegExp(/db\.(a-zA-Z1-9_)*.*/);
 const COLLECTION_FROM_QUERY_REGEX = /(?:db\.)([a-zA-Z1-9_]*)[.]{0,1}/; //extracts the collection name from a valid query
 const FUNCTION_FROM_QUERY_REGEX = /(?:db\.)(?:[a-zA-Z1-9_]*)[.]{1}([a-zA-Z1-9]*)/; //extracts the function from a valid query
-
-const EXTRACT_FULL_QUERY_REGEX = /^(?:db)\..*\..*?\(([^]+.*)\)/; // https://regex101.com/r/dI6kY9/1
-const EXTRACT_QUERY_AND_OPTIONS_REGEX = /({[^]*})(?:[\s\n\r])*,(?:[\s\n\r])*({[^]*})/; // https://regex101.com/r/cY1hO6/1
 
 class QueryParser {
   isValidQuery(query) {
@@ -25,42 +24,19 @@ class QueryParser {
   }
 
   parseQuery(query) {
-    if (!this.isValidQuery(query)) return null;
+    var parts = bracketMatcher.match(query);
 
-    var queryAndOptions = _parseQueryAndOptions(query);
-    var fullQuery = _parseFullQuery(query);
-    fullQuery = fullQuery && fullQuery.trim ? fullQuery.trim() : fullQuery;
+    //handle an valid query that is empty (no brackets)
+    if (this.isValidQuery(query) && (!parts || !parts.length)) return '{}';
 
-    return queryAndOptions && queryAndOptions.query ? queryAndOptions.query : (fullQuery || '{}');
+    return (parts && parts.length) ? parts[0] : null;
   }
 
   parseOptions(query) {
-    if (!this.isValidQuery(query)) return null;
+    var parts = bracketMatcher.match(query);
 
-    var queryAndOptions = _parseQueryAndOptions(query);
-
-    return queryAndOptions ? queryAndOptions.options : null;
+    return (parts && parts.length > 1) ? parts[1] : null;
   }
-}
-
-function _parseFullQuery(rawQuery) {
-  var matches = rawQuery.match(EXTRACT_FULL_QUERY_REGEX);
-  return matches && matches.length <= 2 ? matches[1] : null;
-}
-
-function _parseQueryAndOptions(rawQuery) {
-  var fullQuery = _parseFullQuery(rawQuery);
-
-  if (!fullQuery) return null;
-
-  var matches = fullQuery.match(EXTRACT_QUERY_AND_OPTIONS_REGEX);
-
-  if (!matches || !matches.length || matches.length <= 2) return null;
-
-  return {
-    query: matches[1],
-    options: matches[2]
-  };
 }
 
 module.exports = new QueryParser();
