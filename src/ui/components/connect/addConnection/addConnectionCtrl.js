9b504701a74437d9fe2c3fa44e491a9dbee6c659
Tim Officer <timothyofficer@gmail.com>
2016-01-02 17:20:59 -0500

    add a new ‘Test Connection’ button in connection manager

--- src/ui/components/connect/addConnection/addConnectionCtrl.js
+++ src/ui/components/connect/addConnection/addConnectionCtrl.js
@@ -8,8 +8,24 @@ angular.module('app').controller('addConnectionCtrl', [
   'connectionCache',
   function($scope, $timeout, $log, notificationService, connectionCache) {
     const connectionModule = require('lib/modules/connection');
+    const Connection = require('lib/entities/connection');
 
     $scope.currentSubPage = 'server';
+
+    $scope.testConnectionBtnOptions = {
+      buttonSubmittingIcon: 'icon-left fa fa-spin fa-refresh',
+      buttonSubmittingClass: 'btn-default',
+      buttonSuccessIcon: 'icon-left fa fa-check',
+      buttonErrorIcon: 'icon-left fa fa-remove',
+      buttonDefaultIcon: 'fa fa-bolt',
+      buttonDefaultText: 'Test Connection',
+      buttonDefaultClass: 'btn-default',
+      buttonSubmittingText: 'Connecting...',
+      buttonSuccessText: 'Connected',
+      buttonSuccessClass: 'btn-success',
+      buttonErrorText: 'Error Connecting',
+      buttonErrorClass: 'btn-danger'
+    };
 
     $scope.addConnectionForm = $scope.selectedConnection ? _.extend({
       databaseName: ($scope.selectedConnection.databases && $scope.selectedConnection.databases.length) ? $scope.selectedConnection.databases[0].name : null
@@ -85,6 +101,32 @@ angular.module('app').controller('addConnectionCtrl', [
             });
             $log.log(err);
           });
+        });
+    };
+
+    $scope.testConnection = function($event) {
+      if ($event) $event.preventDefault();
+
+      $scope.isTestingConnection = true;
+
+      let newConnection = new Connection($scope.addConnectionForm);
+
+      var startTime = performance.now();
+
+      newConnection.connect()
+        .then(() => {
+          var ellapsed = (performance.now() - startTime).toFixed(5);
+
+          $timeout(() => {
+            $scope.testConnectionResult = 'success';
+          }, (ellapsed >= 1000 ? 0 : 1000));
+        })
+        .catch(() => {
+          var ellapsed = (performance.now() - startTime).toFixed(5);
+
+          $timeout(() => {
+            $scope.testConnectionResult = 'error';
+          }, (ellapsed >= 1000 ? 0 : 1000));
         });
     };
   }
