f6a8eb7f0d30994f10d1ebb5bc92ea8304ced300
Tim Officer <timothyofficer@gmail.com>
2016-01-02 20:59:01 -0500

    replica set yes/no buttons

---
+++ src/ui/directives/btnBoolean/btnBoolean.js
@@ -0,0 +1,30 @@
+angular.module('app').directive('btnBoolean', [
+  function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: __dirname + '/directives/btnBoolean/btnBoolean.html',
+      require: 'ngModel',
+      scope: {
+        trueValue: '=',
+        falseValue: '='
+      },
+      link: function(scope, element, attrs, ngModelCtrl) {
+        scope.checked = false;
+
+        if (!scope.trueValue) scope.trueValue = 'true';
+        if (!scope.falseValue) scope.falseValue = 'false';
+
+        //take initial model value and set editor with it
+        ngModelCtrl.$formatters.push((modelValue) => {
+          if (modelValue) scope.checked = true;
+          else scope.checked = false;
+
+          return modelValue;
+        });
+
+        //ngModelCtrl.$setViewValue(value);
+      }
+    };
+  }
+]);
