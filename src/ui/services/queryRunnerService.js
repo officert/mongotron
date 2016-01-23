4812cac0af20c146ba6a234beae5a0cb70ac7862
Tim Officer <timothyofficer@gmail.com>
2016-01-23 17:04:30 -0500

    don’t try and parse the js expression manually anymore, just eval the whole expression in a custom scope that has access to the db object, and all collections in the database

--- src/ui/services/queryRunnerService.js
+++ src/ui/services/queryRunnerService.js
@@ -1,71 +1,30 @@
 'use strict';
 
 angular.module('app').service('queryRunnerService', [
-  '$timeout',
-  'notificationService', ($timeout, notificationService) => {
+  () => {
     function QueryRunnerService() {}
     const Promise = require('bluebird');
 
-    const queryModule = require('lib/modules/query');
-    const keyValueUtils = require('src/lib/utils/keyValueUtils');
+    const evaluator = require('lib/modules/evaluator');
 
     QueryRunnerService.prototype.runQuery = function(rawQuery, collections) {
-      let _this = this;
-
       return new Promise((resolve, reject) => {
         if (!rawQuery) return reject(new Error('QueryRunnerService - runQuery() - rawQuery is required'));
         if (!collections) return reject(new Error('QueryRunnerService - runQuery() - collections is required'));
 
-        if (!queryModule.isValidQuery(rawQuery)) return reject(new Error(`Sorry, ${rawQuery} is not a valid query`));
+        let evalScope = {
+          db: {}
+        };
 
-        let collectionName = queryModule.parseCollectionName(rawQuery);
+        collections.forEach(collection => {
+          evalScope.db[collection.name] = collection;
+        });
 
-        let collection = _getCollectionByNameFromRawQuery(collectionName, collections);
-
-        if (!collection) return reject(new Error(`Sorry, ${collectionName} is not a valid collection name`));
-
-        let query;
-
-        queryModule.createQuery(rawQuery)
-          .then((_query) => {
-            query = _query;
-            return collection.execQuery(query);
-          })
-          .then((results) => {
-            let queryResults = {
-              time: results.time,
-              result: results.result,
-              query: query,
-              collection: collection
-            };
-
-            if (queryResults.result && _.isArray(queryResults.result)) {
-              queryResults.keyValueResults = keyValueUtils.convert(queryResults.result);
-            }
-
-            if (query.mongoMethod !== 'find' && query.mongoMethod !== 'aggregate' && query.mongoMethod !== 'count') {
-              notificationService.success(`${query.mongoMethod} was successful`);
-
-              return _this.runQuery(`db.${collectionName}.find()`, collections)
-                .then(resolve)
-                .catch(reject);
-            } else {
-              return resolve(queryResults);
-            }
-          })
-          .catch((err) => {
-            return reject(err);
-          });
+        evaluator.eval(rawQuery, evalScope)
+          .then(resolve)
+          .catch(reject);
       });
     };
-
-    function _getCollectionByNameFromRawQuery(collectionName, collections) {
-      if (!collectionName) return null;
-
-      return _.find(collections, (collection) => {
-        return collection.name && collection.name.toLowerCase && collection.name.toLowerCase() === collectionName.toLowerCase() ? true : false;
-      });
-    }
 
     return new QueryRunnerService();
   }
