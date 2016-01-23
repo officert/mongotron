daf740cac7abc8ee26f8f1298055bb1c4f7b5b39
Tim Officer <timothyofficer@gmail.com>
2016-01-23 17:02:33 -0500

    new autocomplete class

---
+++ src/lib/modules/automcomplete/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const esprima = require('esprima');
+// const escodegen = require('escodegen');
+
+/** @class */
+class AutoComplete {
+  getHintsByValue(currentQuery, currentWord) { /* jshint ignore:line */
+    return {
+      hints: []
+        // value: value
+    };
+  }
+}
+
+/** @exports */
+module.exports = new AutoComplete();
