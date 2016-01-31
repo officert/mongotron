9e3acc94ad2ee414b97b50e1d0ed806a8ac157ac
Tim Officer <timothyofficer@gmail.com>
2016-01-31 18:13:15 -0500

    comment out the autocomplete service - not used

--- src/lib/modules/automcomplete/index.js
+++ src/lib/modules/automcomplete/index.js
@@ -1,17 +1,28 @@
-'use strict';
-
+// 'use strict';
+//
 // const esprima = require('esprima');
-// const escodegen = require('escodegen');
-
-/** @class */
-class AutoComplete {
-  getHintsByValue(currentQuery, currentWord) { /* jshint ignore:line */
-    return {
-      hints: []
-        // value: value
-    };
-  }
-}
-
-/** @exports AutoComplete */
-module.exports = new AutoComplete();
+// // const escodegen = require('escodegen');
+//
+// /** @class */
+// class AutoComplete {
+//   getHintsByValue(currentExpression, currentWord) {
+//     let root;
+//
+//     try {
+//       root = esprima.parse(currentExpression);
+//     } catch (err) {
+//       return {
+//         hints: null,
+//         value: currentExpression
+//       };
+//     }
+//
+//     return {
+//       hints: [],
+//       value: currentExpression
+//     };
+//   }
+// }
+//
+// /** @exports AutoComplete */
+// module.exports = new AutoComplete();
