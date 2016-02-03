018f6d4e188e91fa2adfed3cf5697b4294c4b259
Tim Officer <timothyofficer@gmail.com>
2016-02-03 17:32:57 -0500

    test error from github

--- tests/unit/libs/githubApi/listReleases-test.js
+++ tests/unit/libs/githubApi/listReleases-test.js
@@ -52,30 +52,58 @@ describe('utils', () => {
       });
 
       describe('when owner and repository are passed', () => {
-        let listReleasesStub;
-        let releases = [{
-          foo: 'bar'
-        }];
+        describe('and github api returns with an error', () => {
+          let listReleasesStub;
+          let errMsg = 'something broke with github :(';
 
-        before(() => {
-          listReleasesStub = sandbox.stub(githubApi._api.releases, 'listReleases', (options, callback) => {
-            return callback(null, releases);
+          before(() => {
+            listReleasesStub = sandbox.stub(githubApi._api.releases, 'listReleases', (options, callback) => {
+              return callback(new Error(errMsg));
+            });
+          });
+
+          it('should call githubAPi.releases.listReleases() and return a promise', next => {
+            let owner = 'foobar';
+            let repository = 'barfoo';
+
+            githubApi.listReleases(owner, repository)
+              .catch(error => {
+                should.exist(error);
+                error.message.should.equal(errMsg);
+
+                listReleasesStub.callCount.should.equal(1);
+
+                return next(null);
+              });
           });
         });
 
-        it('should call githubAPi.releases.listReleases() and return a promise', next => {
-          let owner = 'foobar';
-          let repository = 'barfoo';
+        describe('and github api returns with no error', () => {
+          let listReleasesStub;
+          let releases = [{
+            foo: 'bar'
+          }];
 
-          githubApi.listReleases(owner, repository)
-            .then(_releases => {
-              should.exist(_releases);
-              releases.should.equal(_releases);
-
-              listReleasesStub.callCount.should.equal(1);
-
-              return next(null);
+          before(() => {
+            listReleasesStub = sandbox.stub(githubApi._api.releases, 'listReleases', (options, callback) => {
+              return callback(null, releases);
             });
+          });
+
+          it('should call githubAPi.releases.listReleases() and return a promise', next => {
+            let owner = 'foobar';
+            let repository = 'barfoo';
+
+            githubApi.listReleases(owner, repository)
+              .then(_releases => {
+                should.exist(_releases);
+                releases.should.equal(_releases);
+
+                listReleasesStub.callCount.should.equal(1);
+
+                return next(null);
+              });
+          });
         });
       });
     });
