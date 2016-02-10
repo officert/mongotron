337facd38a7880a3d746915c1016d18b7d0f4f7a
Tim Officer <timothyofficer@gmail.com>
2016-02-09 21:27:25 -0500

    release notes

--- src/ui/components/about/aboutCtrl.js
+++ src/ui/components/about/aboutCtrl.js
@@ -4,7 +4,21 @@ angular.module('app').controller('aboutCtrl', [
   '$scope',
   ($scope) => {
     const appConfig = require('src/config/appConfig');
+    const githubApi = require('lib/libs/githubApi');
+    const logger = require('lib/modules/logger');
 
     $scope.version = appConfig.version;
+
+    githubApi.listReleases(appConfig.repositoryOwner, appConfig.repositoryName)
+      .then(releases => {
+        $scope.$apply(() => {
+          $scope.currentRelease = _.findWhere(releases, {
+            tag_name: $scope.version //jshint ignore:line
+          });
+        });
+      })
+      .catch(err => {
+        logger.error(err);
+      });
   }
 ]);
