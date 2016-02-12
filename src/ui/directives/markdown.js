be6b810419a998b7e3f650eb335420046715f5aa
Tim Officer <timothyofficer@gmail.com>
2016-02-12 10:28:38 -0500

    new markdown-compiled directive which will find all anchors, attach a click handler to open externally with shell and connect with Angular

--- src/ui/directives/markdown.js
+++ src/ui/directives/markdown.js
@@ -18,3 +18,40 @@ angular.module('app').directive('markdown', [
     };
   }
 ]);
+
+angular.module('app').directive('markdownCompiled', [
+  '$compile', ($compile) => {
+    const shell = require('shell');
+
+    return {
+      restrict: 'A',
+      scope: {
+        markdownCompiled: '='
+      },
+      link: (scope, element) => {
+        scope.$watch('markdownCompiled', newValue => {
+          if (newValue && newValue !== '') {
+            let html = marked(newValue);
+            let $html = $(html);
+
+            $html.find('a').each((index, el) => {
+              $(el).attr('ng-click', 'openExternal(\'' + el.href + '\', $event)');
+            });
+
+            scope.openExternal = function(url, $event) {
+              if (!url) return;
+
+              if ($event) $event.preventDefault();
+
+              shell.openExternal(url);
+            };
+
+            let compiled = $compile($html)(scope);
+
+            element.html(compiled);
+          }
+        });
+      }
+    };
+  }
+]);
