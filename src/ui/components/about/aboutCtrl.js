88e8caca125746a4cb30ca5cc0213247e8df0781
Tim Officer <timothyofficer@gmail.com>
2016-02-12 10:45:09 -0500

    styles for release notes

--- src/ui/components/about/aboutCtrl.js
+++ src/ui/components/about/aboutCtrl.js
@@ -9,16 +9,25 @@ angular.module('app').controller('aboutCtrl', [
 
     $scope.version = appConfig.version;
 
-    githubApi.listReleases(appConfig.repositoryOwner, appConfig.repositoryName)
-      .then(releases => {
-        $scope.$apply(() => {
-          $scope.currentRelease = _.findWhere(releases, {
-            tag_name: $scope.version //jshint ignore:line
+    _getLatestRelease();
+
+    function _getLatestRelease() {
+      $scope.loading = true;
+
+      githubApi.listReleases(appConfig.repositoryOwner, appConfig.repositoryName)
+        .then(releases => {
+          $scope.$apply(() => {
+            $scope.loading = false;
+            $scope.currentRelease = _.findWhere(releases, {
+              tag_name: $scope.version //jshint ignore:line
+            });
           });
+        })
+        .catch(err => {
+          $scope.loading = false;
+          $scope.error = err && err.message ? err.message : err;
+          logger.error(err);
         });
-      })
-      .catch(err => {
-        logger.error(err);
-      });
+    }
   }
 ]);
